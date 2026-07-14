9179bf4aa2fd52f3df0769c620063844e53f60ec
sora-okinawa <165863455+sora-okinawa@users.noreply.github.com>
2026-07-14 20:53:55 +0900

    Initial Commit

---
+++ smartphone-notification-app/smartphone_notification_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartphone_notification_appApp.swift
+//  smartphone-notification-app
+//
+//  Created by sora.sakamoto on 2026/07/14.
+//
+
+import SwiftUI
+
+@main
+struct smartphone_notification_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ smartphone-notification-appTests/smartphone_notification_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  smartphone_notification_appTests.swift
+//  smartphone-notification-appTests
+//
+//  Created by sora.sakamoto on 2026/07/14.
+//
+
+import Testing
+
+struct smartphone_notification_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
